c99442b9ce6fa2b41f8e007f35da95786f2c119e
Fedor <feodor.a.v.03@gmail.com>
2025-04-14 18:44:48 +0300

    some fix

--- main.py
+++ main.py
@@ -132,4 +132,4 @@ if __name__ == "__main__":
     import uvicorn
 
     logger.info(f"Start the application")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=5)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=4)
