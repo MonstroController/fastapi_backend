ab76f8bf3d4563470dfee378612042027a781f92
Fedor <feodor.a.v.03@gmail.com>
2025-05-15 11:40:31 +0300

    fix requirements

--- app/utils/telegram_bot.py
+++ app/utils/telegram_bot.py
@@ -4,5 +4,5 @@ from app.core.config import settings
 async def notify_admins(text):
     async with aiohttp.ClientSession() as session:
         async with session.post(settings.tg_bot.TELEGRAM_BOT_URL, json={"text": text}) as response:
-            res = await response.json(content_type='text/html') 
+            res = await response.json(content_type=None) 
     return res
